4f3450a72417977349b5edcec7dbdf75d666eafa
Akaroe <75326386+Akaroe@users.noreply.github.com>
2022-04-04 10:54:45 +0200

    script france chrono v1.1

---
+++ chrono+France.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Reddit Place - Chrono Trigger + france
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  GL
+// @author       Akaroe
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://raw.githubusercontent.com/Akaroe/reddit-place-chronotrigger/master/CRONO.ico
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/Akaroe/reddit-place-chronotrigger/master/overlay4.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
