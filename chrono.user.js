7242b3af068efd6cf7524232b451b1e306256087
Akaroe <75326386+Akaroe@users.noreply.github.com>
2022-04-04 10:03:33 +0200

    new script

--- chrono.user.js
+++ chrono.user.js
@@ -1,8 +1,11 @@
 // ==UserScript==
-// @name         Reddit Place - chronotrigger
+// @name         Reddit Place - Chrono Trigger r/place
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @icon         https://media.discordapp.net/attachments/297880074877927435/960255679971594240/unknown.png
+// @description  GL
+// @author       Akaroe
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://raw.githubusercontent.com/Akaroe/reddit-place-chronotrigger/master/CRONO.ico
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
@@ -10,12 +13,12 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/Akaroe/reddit-place-chronotrigger/blob/master/overlay.png";
+            i.src = "https://raw.githubusercontent.com/Akaroe/reddit-place-chronotrigger/master/overlay3.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
         })())
- 
+
     }, false);
- 
+
 }
